2f090259b04cb381427cc49af4f4d6890372ea0a
gragragrace <107156916+gragragrace@users.noreply.github.com>
2023-02-19 14:13:34 -0600

    layout and step1-3 scaffolding prompts
    Basic grid layout and step1-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      base: '0.8rem',
+      xl: '1.15rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {
+        gridTemplateColumns:
+        {
+          '25/75': '25% 75%',
+        }
+      }
   },
   plugins: [],
 }
