6f0530e34678fe0fc3edb4fa7df621ab11fedc04
gragragrace <107156916+gragragrace@users.noreply.github.com>
2023-04-30 21:56:52 -0500

    user instructions

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,9 +7,14 @@ const steps = [
     { title: "1.1 Scope", instructions: "First, specify who you want this script to apply for. Is it a specific SIG? Is it all projects? Take a look at the 'Scope' blocks under Social Structures."
     + "Attach a 'Scope' block to the 'I want this script to apply to' block."},
     { title: "1.2 Detecting the situation", instructions: "Think back to a time when this situation happened. What did the student do and how did you notice it was happening? Using the blocks on the right, build a detector that can help you know when the problem is happening."}],
-    [{ title: "2. Potential Causes", instructions: "Based on your understanding of your students, what do you think is causing this situation? Under Potential Causes, list the common reasons why your student might be struggling with the issue."}],
-    [{ title: "3. Context for Root Cause Diagnosis", instructions: "Using the dropdown, select some context information to help you diagnose the key root cause. When the detector triggers, the context information you selected will be sent to you to help you diagnose exactly which root cause is causing the struggling."}],
-    [{ title: "4. Strategies", instructions: "Formulate a help strategy for the potential cause(s) identified in step 2. If you identified multiple potential causes, design a tailored strategy for each one."}]
+    [{ title: "2. Potential Causes", instructions: "Based on your understanding of your students, what do you think is causing this situation? Under Potential Causes, list the common reasons why your student might be struggling with the issue."},
+    {instructions: "Use the add button on the right to add more potential causes. Use the X button to delete potential causes."},
+    { title: "Example", instructions: "Potential reasons why a student marked the task for writing a research proposal as 'BACKLOGGED' might include lack of clarity for the project, lack of experience with writing research proposals, priority shifted to other tasks etc."}],
+    [{ title: "3. Context for Root Cause Diagnosis", instructions: "Using the dropdown, select some context information to help you diagnose the key root cause. When the detector triggers, the context information you selected will be sent to you to help you diagnose exactly which root cause is causing the struggling."},
+    { title: "Example", instructions: "Say that you selected 'Sprint Log-Summary of Stories' for your script that detects whether your student is overcommitted. When the detector triggers you will receive a summary of the stories in their sprint log, which you can read to help you understand why they're overcommiting."}],
+    [{ title: "4. Strategies", instructions: "Formulate a help strategy for the potential cause(s) identified in step 2. If you identified multiple potential causes, design a tailored strategy for each one."},
+    { title: "Tips", instructions: "1. Leverage context info. E.g. if you decide to address the issue during SIG, think about how you can use your knowledge of the situation and the key cause behind it to help students learn more effectively."},
+    { instructions: "2. If the student is a first-quarter DTR student, consider recommending them to meet with their onboarding mentors."}]
 ]
 
 
@@ -63,12 +68,3 @@ export const Sidebar = () => {
         </div>
     );
 }
-
-                    {/* {y.title === "1. The Detector" && (
-                <div className={styles.textareaContainer}>
-                    <textarea 
-                        placeholder="Enter your description here" 
-                        className={styles.textarea} 
-                    />
-                </div>
-                ))} */}
--- lib/blockly/customBlocks/overcomittedblocks.ts
+++ lib/blockly/customBlocks/overcomittedblocks.ts
@@ -6,36 +6,6 @@ import { javascriptGenerator } from 'blockly/javascript';
 //Sprint log -- this is to enable the user to pull data from the sprint log (e.g. total points spent, total points available)
 // This is equivalent to saying tools.planningTool.totalPoints.spent, tools.planningTool.totalPoints.available
 //TODO: change name
-Blockly.Blocks['sprintLog'] = {
-  init: function () {
-    this.appendDummyInput()
-      .appendField('Sprint log: Total Points')
-      .appendField(
-        new Blockly.FieldDropdown([
-          ['Available', 'Available'],
-          ['spent', 'Committed.total'],
-          // //TODO: change the value of this field
-          // ['Total Points spent on Design in the sprint', 'pointsSpentDesign'], //put this one for now to see what it's like to have more options
-        ]),
-        'Dropdown'
-      )
-      .appendField('in the sprint');
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(110);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  },
-};
-
-//TODO: change name
-javascriptGenerator['sprintLog'] = function (block: Blockly.Block) {
-  var selection = block.getFieldValue('Dropdown');
-
-  // Generate the code to perform the calculation using the value
-  var code = "project.tools.planningLog.totalPoints.points" + selection;
-  return [code, javascriptGenerator.ORDER_RELATIONAL];
-};
 
 //endOfSprint -- this is equivalent to saying sprint.endDay, sprint.startDay
 Blockly.Blocks['sprintTime'] = {
