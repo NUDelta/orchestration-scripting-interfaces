78b565bc50febf58fb181f03405d15fecd975445
gragragrace <107156916+gragragrace@users.noreply.github.com>
2023-05-18 10:35:37 -0500

    setup

--- models/ScriptModel.ts
+++ models/ScriptModel.ts
@@ -1,44 +1,44 @@
 import mongoose from 'mongoose';
 
 export interface OSEScript {
-  Name: String;
-  Desc: String;
-  Applicable_Set: String;
-  //Blockly's Xml + Code generated
-  Detector: [String, String];
-  GeneralContext: [String];
-  RC_C_S: [{
-           RC: String,
-           C: [String],
-           S: String,
-         }];
+  // Name: String;
+  Description: String;
+  // Applicable_Set: String;
+  // //Blockly's Xml + Code generated
+  // Detector: [String, String];
+  // GeneralContext: [String];
+  // RC_C_S: [{
+  //          RC: String,
+  //          C: [String],
+  //          S: String,
+  //        }];
 }
 
 const OSEScriptSchema = new mongoose.Schema<OSEScript>({
-  Name: {
+  // Name: {
+  //   type: String,
+  //   required: true,
+  // },
+  Description: {
     type: String,
     required: true,
   },
-  Desc: {
-    type: String,
-    required: true,
-  },
-  Detector: {
-    type: [String,String],
-    required: true,
-  },
-  GeneralContext: {
-    type: [String],
-    required: true,
-  },
-  RC_C_S: {
-    type: [{
-      RC: String,
-      C: [String],
-      S: String,
-    }],
-    required: true,
-  }
+  // Detector: {
+  //   type: [String,String],
+  //   required: true,
+  // },
+  // GeneralContext: {
+  //   type: [String],
+  //   required: true,
+  // },
+  // RC_C_S: {
+  //   type: [{
+  //     RC: String,
+  //     C: [String],
+  //     S: String,
+  //   }],
+  //   required: true,
+  // }
 });
 
 export default (mongoose.models.OSEScript as mongoose.Model<OSEScript>) ||
