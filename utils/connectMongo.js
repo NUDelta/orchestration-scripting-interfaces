cdccb602a0e04cc0dd7fb223121f6cad0cc53dc5
Jcheck27 <jordan.checkoff@gmail.com>
2023-05-21 18:51:30 -0500

    Update connectMongo.js

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,5 +1,22 @@
-import mongoose from 'mongoose';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://JordanCheckoff:Baseball54@cluster0.mucu7je.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
-
-export default connectMongo;
+export async function connect(command) {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("test").command(command);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
