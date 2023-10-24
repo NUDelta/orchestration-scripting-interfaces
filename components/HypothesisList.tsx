520885e13e0c04dfe1ac9202177d3c8541fe4b12
gragragrace <107156916+gragragrace@users.noreply.github.com>
2023-10-24 14:40:50 -0500

    fixed hypo list

--- components/HypothesisList.tsx
+++ components/HypothesisList.tsx
@@ -13,7 +13,8 @@ const HypothesisList: React.FC = () => {
   ]);
 
   const addHypothesis = () => {
-    setHypotheses([...hypotheses, { title: 'Updated hypothesis', content: 'fill in your updated hypothesis' }]);
+    const newHypothesis = { title: 'Updated hypothesis', content: 'fill in your updated hypothesis' };
+    setHypotheses([newHypothesis, ...hypotheses]);
   };
 
   const updateHypothesis = (index: number, content: string) => {
@@ -33,6 +34,9 @@ const HypothesisList: React.FC = () => {
 
   return (
     <div className="p-2">
+      <button onClick={addHypothesis} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-right">
+        Add New Hypothesis
+      </button>
       {hypotheses.map((hypoData, index) => (
         <Hypothesis
           key={index}
@@ -40,9 +44,6 @@ const HypothesisList: React.FC = () => {
           setHypoContent={(content) => updateHypothesis(index, content)}
         />
       ))}
-      <button onClick={addHypothesis} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-right">
-        Add New Hypothesis
-      </button>
       <button
         className={`fixed left-4 bottom-4 p-2 bg-blue-500 text-white rounded cursor-pointer ${
           popupVisible ? 'hidden' : ''
