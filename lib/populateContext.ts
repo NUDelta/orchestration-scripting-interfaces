0ca3a9f6c28ce6e119845c433a949ec6dde1618b
gragragrace <107156916+gragragrace@users.noreply.github.com>
2023-11-04 20:37:18 -0500

    summary of stories and task

--- lib/populateContext.ts
+++ lib/populateContext.ts
@@ -38,7 +38,65 @@ export function populate_sprint_log_points (OS_object) {
     };
     });
 
-    return { totalObj, studentObjs};
+    return {totalObj, studentObjs};
+}
+
+export function populate_sprint_log_summary_of_stories (OS_object) {
+    let project = OS_object.project;
+    const Stories = project.tools.sprintLog.stories;
+
+    let storiesObjs = Stories.map((story) => {
+      const tasks = story.tasks;
+    
+      let DCategory,
+        TCategory,
+        RCategory = false;
+      let pointsD,
+        pointsT,
+        pointsR = 0;
+    
+      let taskObjs = tasks.map((task) => {
+        // Calculate D T R points for the story by summing Task Categories while checking taskCategory
+        if (task.taskCategory == 'D') {
+          DCategory = true;
+          pointsD += task.pointsAllocated;
+        } else if (task.taskCategory == 'T') {
+          TCategory = true;
+          pointsT += task.pointsAllocated;
+        } else if (task.taskCategory == 'R') {
+          RCategory = true;
+          pointsR += task.pointsAllocated;
+        } else {
+          console.log('task.taskCategory needs to be one of D T and R!');
+        }
+    
+        return {
+          description: task.description, // description of the task
+          expectedRoadblocks: task.expectedRoadblocks, // expected roadblocks for the task (if any)
+          pointsRequired: task.pointsAllocated, // number of points allocated for the task
+          DCategory: DCategory,
+          TCategory: TCategory,
+          RCategory: RCategory,
+          assignee: task.assignee, // student assigned to the task
+          taskStatus: task.taskStatus, // current status of task (blank, in-progress, backlogged, done)
+          pointsSpent: task.pointsSpent, // number of points spent on the task
+        };
+      });
+    
+      return {
+        description: story.description, // description of the story (typically, the risk in project)
+        purpose: story.purpose, // purpose of the story (typically, why the risk is important to address)
+        deliverables: story.deliverables, // description of the deliverable
+        totalPointsRequired: story.totalPointsRequired, // number of points allocated to this story
+        DPointsRequired: pointsD,
+        TPointsRequired: pointsT,
+        RPointsRequired: pointsR,
+        totalPointsSpent: story.totalPointsSpent, // number of points spend so far on this story
+        tasks: taskObjs,
+      };
+    });
+
+    return storiesObjs;
 }
 
 // Takes the title of a context piece and returns the value for it
@@ -57,6 +115,22 @@ export function getContextValue(title, OS_object) {
         let sprintLogPointsContext = populate_sprint_log_points(OS_object)
         let output = 'placeholder'
         return output;
+    } else if (title ==  "Sprint log-Summary of Stories") {
+        let storiesObjs = populate_sprint_log_summary_of_stories(OS_object)
+        let output = storiesObjs[0].description
+        if (storiesObjs.length == 0){
+            return "no stories planned";
+        } else {
+            return output;
+        }
+    } else if (title ==  "Sprint log-Summary of Tasks"){
+        let storiesObjs = populate_sprint_log_summary_of_stories(OS_object)
+        let output = storiesObjs[0].tasks[0].description
+        if (storiesObjs.length == 0){
+            return "no stories planned";
+        } else {
+            return output;
+        }
     } else {
         return "placeholder";
     }
