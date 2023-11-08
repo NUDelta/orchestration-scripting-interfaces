1128d9f04ba6a030ec101d59187ac752ddb0e845
NoodleSoup0 <linhly2025@u.northwestern.edu>
2023-11-08 13:05:49 -0600

    Remove Button

--- components/DiagContext.tsx
+++ components/DiagContext.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import styles from './DiagContext.module.css';
 import { options } from '../lib/context/options';
 
-const Context: React.FC = ({items, setItems, context_lib}) => {
-  const [popupOptions, setPopupOptions] = useState(options.filter(option => !items.some(item => item.title === option)));
+const Context: React.FC = ({ items, setItems, context_lib }) => {
+  const [popupOptions, setPopupOptions] = useState(
+    options.filter((option) => !items.some((item) => item.title === option))
+  );
   const [isPopupOpen, setPopupOpen] = useState(false);
-  console.log('context items', items)
 
   const openPopup = () => {
     setPopupOpen(true);
@@ -16,8 +17,16 @@ const Context: React.FC = ({items, setItems, context_lib}) => {
   };
 
   const addOptionToContext = (option: any) => {
-    setItems([...items, {title: option, data: context_lib[option]}]);
+    setItems([...items, { title: option, data: context_lib[option] }]);
     setPopupOptions(popupOptions.filter((popOption) => popOption !== option));
+  };
+
+  const removeItem = (index) => {
+    const removedItem = items[index];
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+    setPopupOptions([...popupOptions, removedItem.title]); // Add the removed item back to popupOptions
   };
 
   return (
@@ -25,8 +34,18 @@ const Context: React.FC = ({items, setItems, context_lib}) => {
       <h2 className="text-lg font-semibold mb-2">Context</h2>
       <div className={styles.context_space}>
         {items.map((option, index) => (
-          <div key={index} className="bg-blue-200 p-4 rounded-md mb-2">
-            {option.title}:  {option.data}
+          <div
+            key={index}
+            className="bg-blue-200 p-4 rounded-md mb-2"
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            {option.title}: {option.data}
+            <button
+              className="bg-red-500 text-white p-2 rounded-md ml-2"
+              onClick={() => removeItem(index)}
+            >
+              X
+            </button>
           </div>
         ))}
         <button
@@ -39,17 +58,25 @@ const Context: React.FC = ({items, setItems, context_lib}) => {
       {isPopupOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-4 rounded-md">
-            <h3 className="text-lg font-semibold mb-2">Select an option to add</h3>
+            <h3 className="text-lg font-semibold mb-2">
+              Select an option to add
+            </h3>
             <ul>
               {popupOptions.map((option, index) => (
                 <li key={index} className="mb-2">
-                  <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => addOptionToContext(option)}>
+                  <button
+                    className="bg-blue-500 text-white p-2 rounded-md"
+                    onClick={() => addOptionToContext(option)}
+                  >
                     {option}
                   </button>
                 </li>
               ))}
             </ul>
-            <button className="bg-red-500 text-white p-2 rounded-md mt-4" onClick={closePopup}>
+            <button
+              className="bg-red-500 text-white p-2 rounded-md mt-4"
+              onClick={closePopup}
+            >
               Close
             </button>
           </div>
