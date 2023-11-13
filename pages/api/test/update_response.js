c76a982b0e6c0d48bfa93f4b050ceeece762b14e
NoodleSoup0 <linhly2025@u.northwestern.edu>
2023-11-12 18:03:24 -0600

    merge

--- pages/api/test/update_response.js
+++ pages/api/test/update_response.js
@@ -1,37 +1,40 @@
 import connectMongo from '../../../utils/connectMongo';
 import { ObjectId } from 'mongodb';
 
-
 export default async function updateResponse(req, res) {
-    if (req.method === 'PUT') {
-      try {
-        const { _id, gen_context, hypothesisList, description, p5Canvas} = req.body;
-        const genContextObject = JSON.parse(gen_context);
-        const hypothesisListObject = JSON.parse(hypothesisList);
-        const canvasStateObject = JSON.parse(p5Canvas);
+  if (req.method === 'PUT') {
+    try {
+      const { _id, gen_context, hypothesisList, description } = req.body;
+      const genContextObject = JSON.parse(gen_context);
+      const hypothesisListObject = JSON.parse(hypothesisList);
 
-        const updatedResponse = await connectMongo({ findAndModify: "responses", 
-                                                query: { _id: new ObjectId(_id)}, 
-                                                update: {$set: {gen_context: genContextObject,
-                                                                hypothesisList: hypothesisListObject,
-                                                                description: description,
-                                                                p5Canvas: canvasStateObject}}, 
-                                                new: true});
-        // console.log(updatedResponse)
+      const updatedResponse = await connectMongo({
+        findAndModify: 'responses',
+        query: { _id: new ObjectId(_id) },
+        update: {
+          $set: {
+            gen_context: genContextObject,
+            hypothesisList: hypothesisListObject,
+            description: description,
+          },
+        },
+        new: true,
+      });
+      console.log(updatedResponse);
 
-        if (updatedResponse) {
-          // If the document is updated successfully, send the updated document in the response
-          res.status(200).json({ test: updatedResponse });
-        } else {
-          // If the document is not found, send a 404 error response
-          res.status(404).json({ error: 'Response not found' });
-        }
-      } catch (error) {
-        console.log('Error updating response:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      if (updatedResponse) {
+        // If the document is updated successfully, send the updated document in the response
+        res.status(200).json({ test: updatedResponse });
+      } else {
+        // If the document is not found, send a 404 error response
+        res.status(404).json({ error: 'Response not found' });
       }
-    } else {
+    } catch (error) {
+      console.log('Error updating response:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
     // Return a 405 Method Not Allowed error if other HTTP methods are used
     res.status(405).json({ error: 'Method Not Allowed' });
-    }
+  }
 }
